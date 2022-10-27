6f991acd576a954a72e3427cf5f0fa95c7183013
gi-lom <gl.rvg.2013@protonmail.com>
2022-10-27 11:19:20 +0200

    adding files

---
+++ install.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+
+# Messaggio di benvenuto
+echo ""
+echo "**************************"
+echo "* NON UN ALTRO SCIOPERO! *"
+echo "**************************"
+echo ""
+echo "Script che ti avvisa di nuovi scioperi di Trenord e ATM ogni volta che accendi il tuo sistema Linux."
+echo "IMPORTANTE: lo script NON funzionerà con browser installati tramite Flatpak e Snap."
+echo ""
+
+USRN=$(id -u -n)
+USRPATH="/home/$USRN/.local/share"
+
+# Controllo se è root
+if [ "$(id -u)" -eq 0 ]; then
+    USRPATH="/usr/share"
+    echo "!!! ATTENZIONE !!!"
+    echo "Stai eseguendo questo script come root, questo può portare a effetti indesiderati. Scrivi 'ok' per continuare, o qualunque altra cosa per fermare lo script."
+    read decision
+    if [[ "$decision" != "ok" ]]; then
+        exit 1
+    fi
+fi
+
+# Opzione di disinstallazione
+if [ -d "$USRPATH/non-un-altro-sciopero" ]; then
+    echo "Lo script è stato installato in precedenza. Premi d per disinstallarlo, qualunque altro tasto per reinstallarlo."
+    read sel
+    if [ "$sel" = "d" ] || [ "$sel" = "D" ]; then
+        echo ""
+        echo "Disinstallando lo script in $USRPATH/non-un-altro-sciopero..."
+        rm -rf "$USRPATH/non-un-altro-sciopero"
+        cr=$(crontab -l)
+        a=($cr)
+        for ((i = 0; i < ${#a[@]}; i++)); do
+            if [[ "${a[i]}" == "$USRPATH/non-un-altro-sciopero/main.py" ]]; then
+                eval "crontab" "-l" "|" "grep" "-v" "@reboot" "sleep" "120" "&&" "python" "$USRPATH/non-un-altro-sciopero/main.py" "--browser" "${a[i + 2]}" "--path" "$USRPATH/non-un-altro-sciopero" "--error" "True" "|" "crontab" "-" >/dev/null 2>&1
+                eval "crontab" "-l" "|" "grep" "-v" "@hourly" "python" "$USRPATH/non-un-altro-sciopero/main.py" "--browser" "${a[i + 2]}" "--path" "$USRPATH/non-un-altro-sciopero" "|" "crontab" "-" >/dev/null 2>&1
+            fi
+        done
+
+        echo ""
+        echo "Disinstallato!"
+        echo ""
+        exit 0
+    fi
+fi
+
+# Controllo se Python è installato
+pyt_installed=""
+if [[ $(which python) == *"no python in"* ]]; then
+    if [[ $(which python3) == *"no python3 in"* ]]; then
+        echo "Python non è presente nel tuo sistema. Installalo prima di far girare questo script."
+        exit 1
+    else
+        pyt_installed="pip3"
+        if [[ $(which pip) == *"no pip in"* ]]; then
+            echo "Pip non è presente nel tuo sistema. Installalo prima di far girare questo script."
+            exit 1
+        fi
+    fi
+else
+    pyt_installed="pip"
+    if [[ $(which pip) == *"no pip in"* ]]; then
+        echo "Pip non è presente nel tuo sistema. Installalo prima di far girare questo script."
+        exit 1
+    fi
+fi
+
+# Controllo se wget è installato
+if [[ $(which wget) == *"no wget in"* ]]; then
+    echo "Wget non è presente nel tuo sistema. Installalo prima di far girare questo script."
+    exit 1
+fi
+
+# Controllo se il modulo Selenium è installato
+python -c "import selenium" >/dev/null 2>&1
+if [[ $? == 1 ]]; then
+    echo "Installando Selenium..."
+    eval "$pyt_installed" "install" "selenium"
+    if [[ $? == 1 ]]; then
+        echo ""
+        echo "Non è stato possibile installare Selenium. Si suggerisce di avvisare il problema sulla repo: github.com/gi-lom/non-un-altro-sciopero"
+        exit 1
+    fi
+fi
+
+# Controllo se il modulo webdriver-manager è installato
+python -c "import webdriver_manager" >/dev/null 2>&1
+if [[ $? == 1 ]]; then
+    echo "Installando Webdriver Manager..."
+    eval "$pyt_installed" "install" "webdriver_manager"
+    if [[ $? == 1 ]]; then
+        echo ""
+        echo "Non è stato possibile installare Webdriver Manager. Si suggerisce di avvisare il problema sulla repo: github.com/gi-lom/non-un-altro-sciopero"
+        exit 1
+    fi
+fi
+
+# Chiedo quale browser utilizzare
+browser=""
+echo "Quale browser usi? 1) Firefox 2) Chrome 3) Chromium 4) Edge 5) Brave 6) Vivaldi 7) Opera 8) Ungoogled Chromium 9) LibreWolf 0) Chiudi lo script   "
+while [[ "$browser" == "" ]]; do
+    browser_list=()
+    read browser_decision
+    if [[ "$browser_decision" == "1" ]]; then
+        declare -a browser_list=("firefox" "firefox-nightly")
+    elif [[ "$browser_decision" == "2" ]]; then
+        declare -a browser_list=("google-chrome" "google-chrome-beta" "google-chrome-unstable")
+    elif [[ "$browser_decision" == "3" ]]; then
+        declare -a browser_list=("chromium-browser")
+    elif [[ "$browser_decision" == "4" ]]; then
+        declare -a browser_list=("microsoft-edge-stable" "microsoft-edge-beta")
+    elif [[ "$browser_decision" == "5" ]]; then
+        declare -a browser_list=("brave-browser" "brave-browser-beta" "brave-browser-nightly")
+    elif [[ "$browser_decision" == "6" ]]; then
+        declare -a browser_list=("vivaldi-stable vivaldi-snapshot")
+    elif [[ "$browser_decision" == "7" ]]; then
+        declare -a browser_list=("opera-stable" "opera-beta")
+    elif [[ "$browser_decision" == "8" ]]; then
+        declare -a browser_list=("ungoogled-chromium" "ungoogled-chromium-bin")
+    elif [[ "$browser_decision" == "9" ]]; then
+        declare -a browser_list=("librewolf")
+    elif [[ "$browser_decision" == "0" ]]; then
+        echo ""
+        echo "Ciao!"
+        echo ""
+        exit 1
+    else
+        echo "Invia un numero da 0 a 9."
+    fi
+    for b in "${browser_list[@]}"; do
+        res=$(eval "which" "$b")
+        if [[ "$res" != *"no $b in"* ]]; then
+            browser="$res"
+            break
+        fi
+    done
+    if [ "$browser" == "" ]; then
+        echo "Non e' stato trovato il browser selezionato. Selezionane un altro: "
+    else
+        echo "Browser trovato!"
+    fi
+done
+
+# Sposto su .local/share
+echo ""
+echo "Configurando le cartelle su $USRPATH/non-un-altro-sciopero..."
+if [ ! -d "$USRPATH/non-un-altro-sciopero" ]; then
+    mkdir "$USRPATH/non-un-altro-sciopero" >/dev/null 2>&1
+    if [ ! -d "$USRPATH/non-un-altro-sciopero" ]; then
+        echo "Non è stato possibile creare la cartella $USRPATH/non-un-altro-sciopero. Si suggerisce di avvisare il problema sulla repo: github.com/gi-lom/non-un-altro-sciopero"
+        exit 1
+    fi
+    cp -R -v -T src "$USRPATH/non-un-altro-sciopero" >/dev/null 2>&1
+else
+    cp src/main.py "$USRPATH/non-un-altro-sciopero" >/dev/null 2>&1
+fi
+if [ ! -f "$USRPATH/non-un-altro-sciopero/main.py" ]; then
+    echo "Non è stato possibile creare uno o più file nella cartella $USRPATH/non-un-altro-sciopero. Si suggerisce di avvisare il problema sulla repo: github.com/gi-lom/non-un-altro-sciopero"
+    exit 1
+fi
+chmod +x "$USRPATH/non-un-altro-sciopero/main.py" >/dev/null 2>&1
+chmod +x "$USRPATH/non-un-altro-sciopero/checker.txt" >/dev/null 2>&1
+
+# Cancello i crontab precedenti
+echo ""
+echo "Cancellando l'eventuale crontab precedente..."
+cr=$(crontab -l)
+a=($cr)
+for ((i = 0; i < ${#a[@]}; i++)); do
+    if [[ "${a[i]}" == "$USRPATH/non-un-altro-sciopero/main.py" ]]; then
+        eval "crontab" "-l" "|" "grep" "-v" "@reboot" "sleep" "120" "&&" "python" "$USRPATH/non-un-altro-sciopero/main.py" "--browser" "${a[i + 2]}" "--path" "$USRPATH/non-un-altro-sciopero" "--error" "True" "|" "crontab" "-" >/dev/null 2>&1
+        eval "crontab" "-l" "|" "grep" "-v" "@hourly" "python" "$USRPATH/non-un-altro-sciopero/main.py" "--browser" "${a[i + 2]}" "--path" "$USRPATH/non-un-altro-sciopero" "|" "crontab" "-" >/dev/null 2>&1
+    fi
+done
+
+# Creo l'autostart
+echo ""
+echo "Creando il crontab..."
+crontab -l >mycron >/dev/null 2>&1
+echo "@reboot sleep 120 && python $USRPATH/non-un-altro-sciopero/main.py --browser $browser --path $USRPATH/non-un-altro-sciopero --error True" >>mycron
+echo "@hourly python $USRPATH/non-un-altro-sciopero/main.py --browser $browser --path $USRPATH/non-un-altro-sciopero" >>mycron
+crontab mycron >/dev/null 2>&1
+if [[ $? == 1 ]]; then
+    echo ""
+    echo "Non è stato possibile creare il crontab. Si suggerisce di avvisare il problema sulla repo: github.com/gi-lom/non-un-altro-sciopero"
+    exit 1
+fi
+rm mycron
+
+# Fine
+echo ""
+echo "Fatto!"
+echo "Se devi cambiare browser, basta far girare di nuovo questo script."
+echo ""
---
+++ src/main.py
@@ -0,0 +1,127 @@
+import os
+
+from subprocess import run, PIPE
+from time import strftime, localtime
+from argparse import ArgumentParser as argument_parser
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.edge.service import Service as EdgeService
+
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.opera import OperaDriverManager
+
+
+
+def train_checker(driver, website, name, css_element, text_element, path):
+    driver.get(website)
+    try:
+        alwait = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, css_element))
+        )
+        els = driver.find_elements(By.XPATH, text_element)
+        if len(els) > 0:
+            alert = els[0].text.split("\n")
+            alert_title = name + ": " + alert[0].lower()
+            alert_desc = alert[1] if len(alert) > 1 else ""
+            if "sciopero" in alert_title.lower() or "sciopero" in alert_desc.lower():
+                filepath = str(path) + "/ferrovie/"+name+".txt"
+                mode = "r+" if os.path.isfile(filepath) else "w"
+                with open(filepath, mode) as txt:
+                    line = txt.readlines() if mode == "r+" else []
+                    text = alert_title + " " + alert_desc
+                    if len(line) == 0 or line[0] != text:
+                        os.system('notify-send "'+alert_title+'" "'+alert_desc+'"')
+                        txt.seek(0)
+                        txt.truncate()
+                        txt.write(text)            
+    except (NoSuchElementException, TimeoutException) as e:
+        pass
+
+
+def main():
+    # Argument Parser
+    parser = argument_parser()
+    parser.add_argument('--browser', type=str, required=True)
+    parser.add_argument('--path', type=str, required=True)
+    parser.add_argument('--error', type=str)
+    args = parser.parse_args()
+    browser_input = args.browser
+    path = args.path
+
+    try:
+        # Comunica che lo script è stato eseguito in un file
+        with open(str(path) + "/checker.txt", "r+") as checker:
+            checker.seek(0)
+            checker.write("Ho controllato gli scioperi alla data " + strftime('%Y-%m-%d, %H:%M', localtime()))
+            checker.truncate()
+
+        # Verifica che il browser ci sia
+        if len(run(["which", browser_input], stderr=PIPE, text=True).stderr) > 0:
+            os.system(
+                'notify-send "' + "Non un altro sciopero!" + '" "' + "Non è stato trovato il browser indicato quando hai installato lo script. Forse lo hai disinstallato?" + '"')
+            return 1
+
+        # Costruisci il driver
+        options = FirefoxOptions() if "firefox" in browser_input or "librewolf" in browser_input else ChromeOptions()
+        options.headless = True
+        options.binary_location = browser_input
+        driver = None
+        if "google-chrome" in browser_input:
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        elif "brave-browser" in browser_input:
+            driver = webdriver.Chrome(
+                service=ChromeService(ChromeDriverManager(chrome_type=ChromeType.BRAVE).install()), options=options)
+        elif "firefox" in browser_input or "librewolf" in browser_input:
+            driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+        elif "edge" in browser_input:
+            driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
+        elif "opera" in browser_input:
+            if browser_input == "/usr/bin/opera":
+                options.add_argument('allow-elevated-browser')
+            driver = webdriver.Opera(executable_path=OperaDriverManager().install(), options=options)
+        else:
+            driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), options=options)
+
+        # Esegui il driver
+        train_checker(
+            driver,
+            "https://www.trenord.it/",
+            "Trenord",
+            ".container-alert .alert .text",
+            "//*[contains(@class,'alert') and .//*[contains(text(), 'Sciopero')]]",
+            path
+        )
+        train_checker(
+            driver,
+            # 'https://web.archive.org/web/20220626200054/https://www.atm.it/it/Pagine/default.aspx', # test url
+            "https://www.atm.it/it/Pagine/default.aspx",
+            "ATM",
+            "#infomobilita",
+            "//*[contains(@class,'news-item') and .//*[contains(text(), 'Sciopero')]]",
+            path
+        )
+
+        driver.quit()
+
+    except Exception as e:
+        if args.error == "True":
+            os.system(
+                'notify-send "' + "Non un altro sciopero!" + '" "' + "Errore: " + str(e) + '"')
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
